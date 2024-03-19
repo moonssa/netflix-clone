a651f6ba9fc8f70dd76d7a7a6742f03621444845
rebootedCoder2 <oborimoon@gmail.com>
2024-03-19 17:19:59 +0900

    netflix head css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/tv",
+        element: <Tv />,
+      },
+      {
+        path: "search",
+        element: <Search />,
+      },
+    ],
+  },
+]);
 function App() {
   return (
     <div className="App">
